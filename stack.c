aea621874207cdcd29106b83a78b337364eaa2f8
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-08-11 23:44:34 +0530

    edit in c file

--- stack.c
+++ stack.c
@@ -20,14 +20,21 @@ void display(){
 
 int main (){
     int item ,n, k;
+    while (n==0){
+    printf("Enter 1 for push, 2 for pop, 3 for display and 0 for exit");
+    scanf("%d", &n);
     switch (n)
     {
     case 1:
-        /* code */
-        break;
+    if (top == 100)
+    {
+        printf("the stack is overflowed");
+    }
+    
     
     default:
         break;
     }
+}
 
 }
